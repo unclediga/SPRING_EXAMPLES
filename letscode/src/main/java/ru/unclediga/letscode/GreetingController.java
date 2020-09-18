df9059080309dbe5a12aedec9a4e8bc341054a08
homeuser <zowie_i@mail.ru>
2020-09-18 16:55:27 +0300

    add and test Mustache templates

--- letscode/src/main/java/ru/unclediga/letscode/GreetingController.java
+++ letscode/src/main/java/ru/unclediga/letscode/GreetingController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
@@ -12,5 +14,12 @@ public class GreetingController {
                            Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/greetingM")
+    public String greeting2(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                            Map<String, Object> model) {
+        model.put("name", name);
+        return "greetingM";
     }
 }
