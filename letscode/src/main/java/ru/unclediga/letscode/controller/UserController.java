bfc2dbe99b9b5ac8ce3b928096fef467d2e3f21a
homeuser <zowie_i@mail.ru>
2020-10-07 18:11:49 +0300

    letscode: only ADMIN-right user can open path '/user'

--- letscode/src/main/java/ru/unclediga/letscode/controller/UserController.java
+++ letscode/src/main/java/ru/unclediga/letscode/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.unclediga.letscode.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     UserRepo userRepo;
